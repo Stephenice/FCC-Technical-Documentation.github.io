b9d3548029e0cba630732bd1e5cf6dea1784ed8f
stephenice <stephencode@hotmail.com>
2022-05-09 22:24:13 +0200

    Update dynamic items js

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,7 +1,18 @@
 "use strict";
 const tools_box = document.querySelector(".tools_box");
+const html_box = document.querySelector(".html_box");
+const css_box = document.querySelector(".css_box");
+const javascript_box = document.querySelector(".html_box");
+const fonts_box = document.querySelector(".fonts_box");
+const colors_box = document.querySelector(".colors_box");
+const images_box = document.querySelector(".images_box");
+const icons_box = document.querySelector(".icons_box");
+const design_box = document.querySelector(".design_box");
+const blogs_box = document.querySelector(".blogs_box");
+const algorithms_box = document.querySelector(".algorithms_box");
+const youtube_box = document.querySelector(".youtube_box");
+
 const url_link = `../resources/section.json`;
-console.log(url_link);
 
 // fetch data
 fetch(url_link)
@@ -14,29 +25,34 @@ fetch(url_link)
 
 function render_data(data) {
   if (data.tools) {
-    for (let index = 0; index < data.tools.length; index++) {
-      const html = `
-    <div class="resource_box box_items">
-    <div class="box_img box">
-      <img src=" ${data.tools[index].image}" alt="" />
-    </div>
-    <div class="box_header box">
-      <h3>
-        <a
-          href=" ${data.tools[index].url}"
-          target="_blank"
-          > ${data.tools[index].name}</a
-        >
-      </h3>
-    </div>
-    <div class="box_des box">
-      <p class="description">
-        ${data.tools[index].description}
-      </p>
-    </div>
+    render_items(data.tools);
+  }
+}
+
+function render_items(data_items) {
+  console.log(data_items);
+  for (let index = 0; index < data_items.length; index++) {
+    const html = `
+  <div class="resource_box box_items">
+  <div class="box_img box">
+    <img src=" ${data_items[index].image}" alt="" />
   </div>
-    `;
-      tools_box.insertAdjacentHTML("beforeend", html);
-    }
+  <div class="box_header box">
+    <h3>
+      <a
+        href=" ${data_items[index].url}"
+        target="_blank"
+        > ${data_items[index].name}</a
+      >
+    </h3>
+  </div>
+  <div class="box_des box">
+    <p class="description">
+      ${data_items[index].description}
+    </p>
+  </div>
+</div>
+  `;
+    tools_box.insertAdjacentHTML("beforeend", html);
   }
 }
